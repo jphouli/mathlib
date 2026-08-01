9af70302d4171f0c2908390498fd207edcf63420
Jordan Houlihan <jphouli1@gmail.com>
2026-08-01 15:08:37 -0400

    basic structure for a library project

---
+++ include/math_lib/linear_algebra.hpp
@@ -0,0 +1,14 @@
+#ifndef MATH_LIB_LINEAR_ALGEBRA_HPP
+#define MATH_LIB_LINEAR_ALGEBRA_HPP
+
+namespace ml::la {
+  
+  template <typename T>
+  struct vec2 {
+    T x;
+    T y;
+  };
+
+}
+
+#endif
---
+++ test/linear_algebra_test.cpp
@@ -0,0 +1,2 @@
+#include <gtest/gtest.h>
+#include <math_lib/linear_algebra.hpp>
